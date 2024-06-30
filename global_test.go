165226a562af343d6962054beae732a59ed5b60e
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-06-30 18:04:58 -0300

    test: new tests for global functions

--- global_test.go
+++ global_test.go
@@ -1,3 +1,142 @@
 package goinject_test
 
-// TODO: tests for global.go
+import (
+	"errors"
+	"testing"
+
+	goinject "github.com/d1360-64rc14/go-inject"
+)
+
+func TestRegisterType(t *testing.T) {
+	t.Run("Normal execution", func(t *testing.T) {
+		err := recoverPanic(func() {
+			goinject.RegisterType[TestA, *TestAImpl]()
+		})
+
+		if err != nil {
+			t.Errorf("unexpected error: '%v'", err)
+		}
+	})
+
+	t.Run("Already registered", func(t *testing.T) {
+		err := recoverPanic(func() {
+			goinject.RegisterType[TestA, *TestAImpl]()
+		})
+
+		if !errors.Is(err, goinject.ErrAlreadyRegistered) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrAlreadyRegistered, err)
+		}
+	})
+
+	t.Run("Wrong abstract type", func(t *testing.T) {
+		err := recoverPanic(func() {
+			goinject.RegisterType[string, *TestBImpl]()
+		})
+
+		if !errors.Is(err, goinject.ErrNotAnInterface) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrNotAnInterface, err)
+		}
+	})
+
+	t.Run("Wrong concrete type", func(t *testing.T) {
+		err := recoverPanic(func() {
+			goinject.RegisterType[TestC, string]()
+		})
+
+		if !errors.Is(err, goinject.ErrNotAnStruct) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrNotAnStruct, err)
+		}
+	})
+}
+
+func TestRegister(t *testing.T) {
+	t.Run("Normal execution", func(t *testing.T) {
+		inst := &TestCImpl{}
+
+		err := recoverPanic(func() {
+			goinject.Register[TestC](inst)
+		})
+
+		if err != nil {
+			t.Errorf("unexpected error: '%v'", err)
+		}
+	})
+
+	t.Run("Already registered", func(t *testing.T) {
+		inst := &TestCImpl{}
+
+		err := recoverPanic(func() {
+			goinject.Register[TestC](inst)
+		})
+
+		if !errors.Is(err, goinject.ErrAlreadyRegistered) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrAlreadyRegistered, err)
+		}
+	})
+
+	t.Run("Wrong abstract type", func(t *testing.T) {
+		inst := ""
+
+		err := recoverPanic(func() {
+			goinject.Register[string](inst)
+		})
+
+		if !errors.Is(err, goinject.ErrNotAnInterface) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrNotAnInterface, err)
+		}
+	})
+}
+
+func TestInject(t *testing.T) {
+	t.Run("Normal execution", func(t *testing.T) {
+		var inst TestA
+
+		err := recoverPanic(func() {
+			goinject.Inject(&inst)
+		})
+
+		if err != nil {
+			t.Errorf("unexpected error: '%v'", err)
+		}
+	})
+
+	t.Run("Wrong abstract type", func(t *testing.T) {
+		var inst *TestAImpl
+
+		err := recoverPanic(func() {
+			goinject.Inject(&inst)
+		})
+
+		if !errors.Is(err, goinject.ErrNotAnInterface) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrNotAnInterface, err)
+		}
+	})
+
+	t.Run("Injected object", func(t *testing.T) {
+		var inst TestA
+
+		err := recoverPanic(func() {
+			goinject.Inject(&inst)
+		})
+
+		if inst == nil {
+			t.Errorf("expected instance, got nil")
+		}
+
+		if err != nil {
+			t.Errorf("unexpected error: '%v'", err)
+		}
+	})
+
+	t.Run("Not registered type", func(t *testing.T) {
+		var inst TestE
+
+		err := recoverPanic(func() {
+			goinject.Inject(&inst)
+		})
+
+		if !errors.Is(err, goinject.ErrNoConcreteTypeSupplied) {
+			t.Errorf("expected error: '%v', got '%v'", goinject.ErrNoConcreteTypeSupplied, err)
+		}
+	})
+}
