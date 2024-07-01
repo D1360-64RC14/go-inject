d89131b3f5715ff54ea854d19e1003c3e3949658
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-06-30 21:42:08 -0300

    docs: DIContainer and InitializableDependency interfaces

--- container.go
+++ container.go
@@ -4,12 +4,16 @@ import (
 	"reflect"
 )
 
+// DIContainer declares what must be implemented by the struct type to be
+// used by the global functions.
 type DIContainer interface {
 	RegisterType(abstractType reflect.Type, concreteType reflect.Type)
 	Register(abstractType reflect.Type, concreteInstance any)
 	Inject(abstractType reflect.Type) any
 }
 
+// InitializableDependency declares the [InitializableDependency.Initialize]
+// contract that can be called during the registration process by the [DefaultContainer].
 type InitializableDependency interface {
 	Initialize()
 }
