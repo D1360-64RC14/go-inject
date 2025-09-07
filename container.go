4380634f04d7a79f65e9bbcbced6226980c66e46
D1360-64RC14 <gdiego416@gmail.com>
2025-09-07 19:13:47 -0300

    docs: document DIContainer and InitializableDependency methods

--- container.go
+++ container.go
@@ -7,8 +7,29 @@ import (
 // DIContainer declares what must be implemented by the struct type to be
 // used by the global functions.
 type DIContainer interface {
+	// RegisterType of an abstract type to a concrete type inside the DI
+	// container, to be injected later.
+	//
+	// The Abstract type must be an interface, and the Concrete type must be a
+	// struct type that implements the interface. Anything different from this
+	// must panic.
 	RegisterType(abstractType reflect.Type, concreteType reflect.Type)
+
+	// Register an abstract type to a concrete instance inside the DI
+	// container, to be injected later.
+	//
+	// The Abstract type must be an interface, and the object instance must be
+	// the type of a struct that implements the interface. Anything different
+	// from this must panic.
 	Register(abstractType reflect.Type, concreteInstance any)
+
+	// Inject the instance of the registered Concrete type from the DI container.
+	//
+	// The Abstract type must be an interface.
+	//
+	// The Concrete type must be instantiated if it isn't already. The
+	// [InitializableDependency.InitializeDependency] method must be be called
+	// if the Concrete type implements the [InitializableDependency] interface.
 	Inject(abstractType reflect.Type) any
 }
 
@@ -16,5 +37,7 @@ type DIContainer interface {
 // [InitializableDependency.InitializeDependency] contract that can be called
 // during the registration process by the [BaseContainer].
 type InitializableDependency interface {
+	// InitializeDependency after the instance is created, from the
+	// [DIContainer.Inject] method.
 	InitializeDependency()
 }
